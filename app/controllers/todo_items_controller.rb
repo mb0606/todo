7c2692fc69e3b018b3c11603a003472bde4a47ca
Marco Berardini <mberardini@evolution-man.com>
2015-08-19 12:48:33 -0700

    Able to mark todo item as complete

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,6 +1,7 @@
 class TodoItemsController < ApplicationController
 
 	before_action  :set_todo_list
+	before_action :set_todo_item, except: [:create]
 
 	def new
 		@todo_item = @todo_list.todo_items.new
@@ -22,13 +23,19 @@ class TodoItemsController < ApplicationController
 
 
 	def destroy
-		@todo_item = @todo_list.todo_items.find(params[:id])
+
 		if @todo_item.destroy
 			flash[:success] = "Todo item successfully deleted"
 		else 
 			flash[:error] = "Todo item not deleted"
 		end
 		redirect_to @todo_list
+	end
+
+	def complete
+		@todo_item.update_attribute(:completed_at, Time.now)
+		redirect_to @todo_list, notice: "Todo item completed"
+
 	end
 
 
@@ -41,6 +48,10 @@ class TodoItemsController < ApplicationController
 
 	def set_todo_list
 		@todo_list = TodoList.find(params[:todo_list_id])
+	end
+
+	def set_todo_item
+		@todo_item = @todo_list.todo_items.find(params[:id])
 	end
 
 	def todo_item_params
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,4 +2,9 @@ class TodoItem < ActiveRecord::Base
   	belongs_to :todo_list
 
     validates :content, presence: true
+
+    def completed?
+    	!completed_at.blank?
+    end
+
 end
